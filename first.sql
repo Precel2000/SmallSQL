fde3c8dd75f14c1f3c0698b7e72fa6dc3056ddd8
Dominika Daraż <42532942+Precel2000@users.noreply.github.com>
2020-05-10 15:41:17 +0200

    create a table and insert values

---
+++ first.sql
@@ -0,0 +1,14 @@
+ CREATE TABLE celebs (
+   id INTEGER, 
+   name TEXT, 
+   age INTEGER
+);
+
+ INSERT INTO celebs (id, name, age)
+ VALUES 
+ (1, 'Justin Bieber', 22), 
+ (2, 'Beyonce Knowles', 33),
+ (3, 'Jeremy Lin', 26),
+ (4, 'Taylor Swift', 26);
+ 
+ SELECT * FROM celebs;
